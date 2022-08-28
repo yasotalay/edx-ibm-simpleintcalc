ed0265b7b3a8549fa5affcf6871ea48308b71095
yasotalay <111748398+yasotalay@users.noreply.github.com>
2022-08-28 15:41:35 +0300

    fix files

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 function compute()
 {
-   var principal = document.getElementById("principal").value;
-   if(parseInt(principal) < 1){
-       alert("Enter a positive number");
-       document.getElementById("principal").focus();
-       return;
-   }
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br>at an interest rate of "+rate+"%\<br\>You will recieve an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+		//Create variables from input
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    
+    //do math
+    let interest = principal * years * rate / 100;
+    let endYear = new Date().getFullYear() + parseInt(years);
+
+    //compound interest formula
+    let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
+		
+    //validate the principal. If negative or 0 alert user and focus on principal
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    
+    //display result
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
+    <br>With monthly compounded interest your balance would instead be <mark>${compounded.toFixed(2)}</mark>`;
 }
 
-function updateRate(){
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-   
+//display rate % by slider
+function rangeReader()
+{
+    let sliderValue = document.getElementById("rate").value;
+    document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
